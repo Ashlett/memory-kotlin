0dc4fba3ee158353bb3e26597a523b84f09fe762
Alicja Kozikowska <alicja.kozikowska@thermondo.de>
2022-02-24 16:32:35 +0100

    Update gradle

--- app/src/main/java/com/ashlett/memory/ItemAdapter.kt
+++ app/src/main/java/com/ashlett/memory/ItemAdapter.kt
@@ -3,7 +3,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
-import com.ashlett.memory.GameActivity
 import com.ashlett.memory.Item
 import com.ashlett.memory.R
 
