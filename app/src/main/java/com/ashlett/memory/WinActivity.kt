50e3a116ba228034662c02c41324ac277b572b9f
Alicja Kozikowska <alicja.kozikowska@thermondo.de>
2022-03-17 18:11:50 +0100

    Use konfetti defaults

--- app/src/main/java/com/ashlett/memory/WinActivity.kt
+++ app/src/main/java/com/ashlett/memory/WinActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ashlett.memory.databinding.ActivityWinBinding
 import nl.dionsegijn.konfetti.core.Party
-import nl.dionsegijn.konfetti.core.Position
 import nl.dionsegijn.konfetti.core.emitter.Emitter
 import java.util.concurrent.TimeUnit
 
@@ -15,11 +14,7 @@ class WinActivity : AppCompatActivity() {
     private val party = Party(
         speed = SPEED,
         maxSpeed = MAX_SPEED,
-        damping = DAMPING,
-        spread = SPREAD,
-        colors = listOf(YELLOW, RED, PINK, VIOLET),
         emitter = Emitter(duration = DURATION_MS, TimeUnit.MILLISECONDS).max(MAX_AMOUNT),
-        position = Position.Relative(POSITION_X, POSITION_Y)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,16 +34,8 @@ class WinActivity : AppCompatActivity() {
     companion object {
         private const val SPEED = 0f
         private const val MAX_SPEED = 30f
-        private const val DAMPING = 0.9f
-        private const val SPREAD = 360
-        private const val YELLOW = 0xfce18a
-        private const val RED = 0xff726d
-        private const val PINK = 0xf4306d
-        private const val VIOLET = 0xb48def
         private const val DURATION_MS = 100L
         private const val MAX_AMOUNT = 100
-        private const val POSITION_X = 0.5
-        private const val POSITION_Y = 0.3
 
         fun createIntent(ctx: Context): Intent =
             Intent(ctx, WinActivity::class.java)
